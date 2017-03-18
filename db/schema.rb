8c9b611f2946da5df5d5f821df10f6855e63cf8b
Isa85 <isabella.scharf@aon.at>
2017-03-18 11:08:51 +0100

    4.2 changed datatype price to integer

--- db/schema.rb
+++ db/schema.rb
@@ -19,7 +19,7 @@ ActiveRecord::Schema.define(version: 20170317133258) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.string   "colour"
-    t.string   "price"
+    t.integer  "price"
   end
 
 end
