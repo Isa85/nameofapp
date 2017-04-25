40f58e99110d2c37f9dd5287d9f10f1146cfa3cd
Isa85 <isabella.scharf@aon.at>
2017-04-25 09:13:34 +0200

    5.7 created comment model, set up two scopes, styled product show page

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,9 +1,18 @@
 class Product < ApplicationRecord
 	has_many :orders
-def self.search(search_term)
-  Product.where("name LIKE ?", "%#{search_term}%")
-else
+	has_many :comments
+	def self.search(search_term)
+  	Product.where("name LIKE ?", "%#{search_term}%")
+	else
 	Product.where("name ilike ?", "%#{search_term}%")
-end
+	end
+
+	def highest_rating_comment
+  		comments.rating_desc.first
+	end
+	def lowest_rating_comment
+		comments.rating_asc.first
+	end
+
 end
 
