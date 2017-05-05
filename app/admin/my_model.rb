33fe469688329c19cc080a3d14f1d850458d6bb2
Isa85 <isabella.scharf@aon.at>
2017-05-05 09:31:37 +0200

    5.11 changed myModel.rb

--- app/admin/my_model.rb
+++ app/admin/my_model.rb
@@ -1,16 +1,17 @@
-ActiveAdmin.register MyModel do
+#ActiveAdmin.register MyModel do
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-#
- #permit_params :list, :of, :attributes, :on, :model
+
+#permit_params :title, :content, :publisher_id, role_ids: []
+#permit_params :list, :of, :attributes, :on, :model
 #permit_params :title, :content, :publisher_id
 #
 # or
 #
- permit_params do
-   permitted = [:permitted, :attributes]
-   permitted << :other if params[:action] == 'create' && current_user.admin?
-   permitted
- end
+ #permit_params do
+ #  permitted = [:permitted, :attributes]
+ #  permitted << :other if params[:action] == 'create' && current_user.admin?
+ #  permitted
+# end
 
-end
+#end
