88a77f283ffc1120ed72b2cdcd80132720872252
Isa85 <isabella.scharf@aon.at>
2017-09-14 20:02:25 +0200

    fixing erros

--- config/routes.rb
+++ config/routes.rb
@@ -1,29 +1,25 @@
 Rails.application.routes.draw do
-  devise_for :admin_users, ActiveAdmin::Devise.config
-   ActiveAdmin.routes(self)
-    devise_for :users, :path => '', :path_names => {:sign_in => 'login', :sign_out => 'logout'}, :controllers => { :registrations => "registrations" }
-    resources :users
-    resources :products do
+
+  devise_for :users, :path => '', :path_names => {:sign_in => 'login', :sign_out => 'logout'}, :controllers => { :registrations => "registrations" }
+  resources :users
+
+  resources :products do
     resources :comments
-    mount ActionCable.server => '/cable'
   end
+  
+  mount ActionCable.server => '/cable'
   
   resources :orders, only: [:index, :show, :create, :destroy]
 
   root 'static_pages#landing_page'
 
   get 'static_pages/index'
-
   get 'static_pages/about'
-
   get 'static_pages/contact'
 
   post 'static_pages/thank_you'
-
   post 'payments/create'
 
   #get 'static_pages/thank_you'
-
   #get 'static_pages/landing_page'
-
 end
