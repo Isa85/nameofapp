a164b7003dc4588c15fa326b32383503ea8248e1
Isa85 <isabella.scharf@aon.at>
2017-09-17 12:56:28 +0200

    6.14: ActiveJob

--- app/channels/product_channel.rb
+++ app/channels/product_channel.rb
@@ -6,5 +6,13 @@ class ProductChannel < ApplicationCable::Channel
   def unsubscribed
     # Any cleanup needed when channel is unsubscribed
   end
+  def listen (data)
+    stop_all_streams
+    stream_for data['product_id']
+  end
+
+  
 end
+
+
 ActionCable.server.broadcast 'product_channel', comment: "Hello World", average_rating: 5
