ae72ae0bcdd0515bf1e573ef95c8a5d8c48b9911
Isa85 <isabella.scharf@aon.at>
2017-09-19 19:46:44 +0200

    6.12 redis

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -2,6 +2,14 @@ module ProductsHelper
   def cache_key_for_products
     count          = Product.count
     max_updated_at = Product.maximum(:updated_at).try(:utc).try(:to_s, :number)
+    
+    q = ""
+    if params[:q]
+      q = params[:q]
+    elsif (params[:aq] && !params[:aq].strip.empty?) || params[:has_carousel_picture]
+      q = "#{params[:aq]}-#{params[:has_carousel_picture]}"
+    end
+
     "products/all-#{count}-#{max_updated_at}"
   end
 end
