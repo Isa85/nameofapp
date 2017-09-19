faeafd02a2971750bf834a5f700ba5712c0b7b0e
Isa85 <isabella.scharf@aon.at>
2017-09-19 19:49:12 +0200

    redis

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,28 +1,15 @@
 class StaticPagesController < ApplicationController
   def index
   end
-  
+
   def landing_page
-    @products = Product.where("carousel_image LIKE '%-%'").last(20)
-    @products.sort_by! { |p| -p.hits }
-    @products = @products[0..3]
-
-    render layout: "landing"
-  end
-
-  #def landing_page
-    #@featured_product = Product.first
-  #end
-  
-  def thank_you
-  @name = params[:name]
-  @email = params[:email]
-  @message = params[:message]
-  ActionMailer::Base.mail(:from => @email,
-      :to => 'dposnanski@ymail.com',
-      :subject => "A new contact form message from #{@name}",
-      :body => @message).deliver_now
+  	@products = Product.limit(3)
   end
   
+  	def thank_you
+    @name = params[:name]
+    @email = params[:email]
+    @message = params[:message]
+    UserMailer.contact_form(@email, @name, @message).deliver_now
+  end
 end
-
