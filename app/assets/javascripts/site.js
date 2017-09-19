ae70dd50f2b2323c95a36672099ae84cf2187ede
Isa85 <isabella.scharf@aon.at>
2017-09-19 17:23:56 +0200

    fixing errors

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -42,9 +42,6 @@ $(document).on('turbolinks:load', function() {
 
 var refreshRating = function() {
   $('.rating').raty({
-    starHalf: '<%= asset_path 'star-half.png' %>',
-    starOff: '<%= asset_path 'star-off.png' %>',
-    starOn: '<%= asset_path 'star-on.png' %>',
     scoreName: 'comment[rating]'
   });
   $('.rated').raty({
